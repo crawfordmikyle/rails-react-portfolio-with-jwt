ebe68c40fc298157774e6590627f9683e3bd0a05
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-10-23 12:42:38 -0700

    working on projects controller

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -2,6 +2,28 @@ class Api::ProjectsController < ApplicationController
   before_action :authenticate_api_user
 
   def index
-    render :json => {"message": "got it"} 
+    projects = Projects.all
+    render :json => projects
   end 
+
+  def create
+
+  end
+
+  def destroy
+    set_project
+    if @project
+      @project.delete
+      render :json => {"message" : "project deleted"}, status: 200
+    else 
+      remder :json => {"message" : "I can't find that project"}, status: 400
+    end
+  end  
+
+  private
+
+  def set_project
+    @project = Project.find_by_id(params[:id])
+  end 
+
 end
