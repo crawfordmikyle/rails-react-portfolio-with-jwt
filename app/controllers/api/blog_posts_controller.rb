db161e7cdc9ccf66e135c21fed67a0f046576fca
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-10-24 16:35:07 -0700

    working on controllers

--- app/controllers/api/blog_posts_controller.rb
+++ app/controllers/api/blog_posts_controller.rb
@@ -1,9 +1,32 @@
 class Api::BlogPostsController < ApplicationController
-before_action :authenticate_api_user
+  before_action :authenticate_user, except: [:index]
 
-def index
-  blog_posts = BlogPost.all
-  render json: blog_posts, status: 200
-end 
+  def index
+    blog_posts = BlogPost.all
+    render json: blog_posts, status: 200
+  end 
+
+  def create
+    blog_post = BlogPost.create(blog_post_params)
+    blog_post.save
+  end 
+
+  def update
+
+  end 
+
+  def destroy
+
+  end 
+
+  private
+
+  def set_blog_post
+
+  end 
+
+  def blog_post_params
+
+  end 
 
 end
--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,5 +1,5 @@
 class Api::ProjectsController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user, except: [:index]
 
   def index
     projects = Project.all
