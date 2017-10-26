fbd36f310d53c376428d876adde1c61f5f593b41
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-10-25 17:58:17 -0700

    adding nav

---
+++ client/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+const Nav = () => {
+  return(
+    <div className="nav">
+     <ul className="nav ul">
+      <li className="nav li">Home</li>
+      <li>About</li>
+      <li>Projects</li>
+      <li>Blog</li>
+      <li>Contact</li>
+     </ul>
+    </div>
+  )
+}
+
+export default Nav
--- client/src/containers/app.js
+++ client/src/containers/app.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
 import Landing from '../components/Landing'
+import Nav from '../components/Nav'
 class App extends Component{
   render(){
     return(
       <div>
         <Landing/>
+        <Nav/>
         <h1>Welcome</h1>
       </div>
     )
