256391b58093512af32a186dcd1613f9bbcb0a09
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-10-26 14:06:27 -0700

    working on landing styling

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,9 +3,15 @@ import './landing.css'
 const Landing = () => {
   return(
     <div className="landing">
-      <img id="headshot" src="https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/18402888_10211333915857890_5938430278944942552_n.jpg?oh=a7ab968a7a1820d6578fe3af22b987f3&oe=5A77831F"/>
-      <h1>Mikyle A. Crawford</h1>
-      <h3>Full Stack Developer</h3>
+      <div className="imgDiv">
+        <img id="headshot" src="https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/18402888_10211333915857890_5938430278944942552_n.jpg?oh=a7ab968a7a1820d6578fe3af22b987f3&oe=5A77831F"/>
+      </div>
+      <break></break>
+      <div className="textDiv">
+        <h1 id="name">Mikyle A. Crawford</h1>
+        <br/>
+        <h3 id="title">Full Stack Developer</h3>
+      </div>
     </div>
   )
 }
