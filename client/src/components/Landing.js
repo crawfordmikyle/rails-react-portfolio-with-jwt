4ed74018ec466f2ed219ae7f27a684609e9521a2
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-10-25 17:38:38 -0700

    starting work on main layout

---
+++ client/src/components/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const Landing = () => {
+  return(
+    <div>
+      <h1>Landing</h1>
+    </div>
+  )
+}
+
+export default Landing
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/app';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 import {createStore, applyMiddleware, combineReducers} from 'redux';
