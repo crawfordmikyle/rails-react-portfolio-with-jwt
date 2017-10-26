a902fc24ec22c5f063becbf88214fedf351513eb
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-10-25 18:29:12 -0700

    added skeleton css

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './skeleton.css'
+import './normalize.css'
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
