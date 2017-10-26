c0ad8fafce785720752c060eddd8db928472d4ab
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-10-26 16:48:26 -0700

    working on some of the core list continers

---
+++ client/src/actions/blogPostActions.js
@@ -0,0 +1,4 @@
+// Action Creators
+
+
+// Asynchronous Actions
---
+++ client/src/containers/ProjectsContainer.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+class ProjectsContainer extends Component {
+
+}
+
+export default connect(null)(ProjectsContainer)
