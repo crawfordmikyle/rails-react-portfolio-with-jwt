1cd1b96beacd6766ebd5c272d78b04942429cceb
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-10-27 08:21:29 -0700

    working on main containers

--- client/src/actions/blogPostActions.js
+++ client/src/actions/blogPostActions.js
@@ -1,4 +1,19 @@
 // Action Creators
+const getBlogPosts = (blogPosts) => {
+  return({
+    type: GET_BLOG_POSTs,
+    blogPosts: blogPosts,
+  })
+}
 
 
 // Asynchronous Actions
+
+export const getBlogPostsApi = () => {
+  return dispatch => {
+    return fetch("/api/blog_posts")
+    .then(responce => responce.json)
+    .then(blogPosts => dispatch(getBlogPosts(blogPosts)))
+    .catch(error => console.log(error))
+  }
+}
--- client/src/containers/BlogPostsContainer.js
+++ client/src/containers/BlogPostsContainer.js
@@ -1,5 +1,26 @@
 import React, {Component} from 'react'
+import connect from 'react-redux'
+import getBlogPostsApi from '../actions/blogPostActions'
 
 class BlogPostContainer extends Component {
-  
+  componentDidMount(){
+    debugger
+  }
+
+  render(){
+    return(
+      <div>
+
+      </div>
+
+    )
+  }
 }
+
+const mapStateToProps = (state) =>{
+  return({
+    state
+  })
+}
+
+export connect(null,getBlogPostsApi)(BlogPostContainer)
