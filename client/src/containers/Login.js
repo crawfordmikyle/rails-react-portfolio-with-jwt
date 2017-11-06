29316c47fb9994525b4e83c682343cc405063951
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-11-06 13:01:24 -0800

    working on login

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -1,6 +1,20 @@
 import React, {Component} from 'react'
 
 class Login extends Component {
+  constructor(){
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+  
+  handleOnChange(event){
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
   handleLogin(){
 
   }
@@ -9,11 +23,11 @@ class Login extends Component {
     return(
       <div>
         <form>
-          <label for="email">Email</label>
-          <input type="email" id="email"/>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" value={this.state.email} onChange={(event) => this.handleOnChange(event)}/>
 
-          <label for="password">Password</label>
-          <input type="password" id="password"/>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" value={this.state.password} onChange={(event) => this.handleOnChange(event)}/>
           <br/>
           <input type="submit" style={{margin: "10px 10px 10px 0px"}}/>
         </form>
