e6e3d9ad0fe1e44a3317a468bc78273c58c0945c
Mikyle A Crawford <crawfordmikyle@gmail.com>
2017-12-07 16:42:22 -0800

    working on admin page

--- client/src/components/admin/AdminProjectsList.js
+++ client/src/components/admin/AdminProjectsList.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import AdminProjectsContainer from '../../containers/AdminProjectsContainer'
 const AdminProjectsList = () => {
   return(
     <div>
       <h2>Admin Projects List</h2>
+      <AdminProjectsContainer/>
     </div>
   )
 }
---
+++ client/src/containers/AdminProjectsContainer.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {getProjectsApi} from '../actions/projectActions'
+
+class AdminProjectsContainer extends Component {
+  componentDidMount(){
+    this.props.getProjectsApi()
+  }
+
+  render(){
+    return(
+      <div className="AdminProjectsContainer">
+        
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return({
+    projects: state.projects
+  })
+}
+
+export default connect(mapStateToProps,{getProjectsApi})(AdminProjectsContainer)
