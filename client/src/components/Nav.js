19a892644cde9e8568e391a3d72f044aff1445f2
Mikyle A Crawford <crawfordmikyle@gmail.com>
2018-01-22 11:57:59 -0800

    working on css

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return(
-    <div className="nav">
+    <nav>
      <ul className="nav ul">
       <li><Link to="/">Home</Link></li>
       <li><Link to="/">About</Link></li>
@@ -11,7 +11,7 @@ const Nav = () => {
       <li><Link to="/">Contact</Link></li>
       <li><Link to="/admin">Login</Link></li>
      </ul>
-    </div>
+    </nav>
   )
 }
 
--- client/src/components/main/About.js
+++ client/src/components/main/About.js
@@ -3,34 +3,13 @@ import React from 'react'
 const About = () => {
   return(
     <div className="about">
-      <div className='sectionHeadding'>
-        <h4>About Me</h4>
-      </div>
+      <h4 className='sectionHeading'>About Me</h4>
       <div className="aboutContent">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla malesuada erat, et hendrerit enim malesuada eget. 
-          Nulla felis ipsum, vulputate in risus sodales, vulputate luctus tellus. Vivamus felis justo, egestas id elementum in, euismod 
-          varius ipsum. Quisque aliquam dolor eget euismod accumsan. Ut convallis molestie tortor vitae varius. Phasellus et volutpat leo. 
-          Quisque convallis ultrices consequat.
-          Nam lacinia nulla sit amet sem tristique fringilla. Nunc suscipit iaculis venenatis. Mauris gravida, augue at ultricies aliquam, 
-          lectus sem vulputate dui, iaculis tempus mi velit eget urna. Suspendisse auctor eu elit a imperdiet. Sed quis metus eget ex facilisis 
-          suscipit. Mauris eu justo et elit dictum efficitur non fringilla enim. Phasellus volutpat molestie ante a malesuada.
-          Praesent ultrices, mi in tempor aliquam, ante lacus cursus massa, a gravida lectus magna vehicula metus. Aenean cursus ultricies commodo. 
-          teger a felis nec orci dapibus interdum id quis purus. Nulla facilisi. Cras mattis urna vehicula erat fermentum volutpat. 
-          Vestibulum congue lobortis massa, a feugiat nibh accumsan quis. Nam enim neque, interdum a maximus et, sagittis sed neque. 
-          Pellentesque dictum bibendum eros sit amet eleifend. In hac habitasse platea dictumst. Maecenas et tempus mi. Fusce id dolor 
-          placerat, scelerisque orci ut, fermentum diam. Proin quis justo semper, lacinia est in, convallis lacus. Praesent a 
-          tortor sit amet magna finibus fermentum et vitae quam.
-          Phasellus vitae elementum urna. Donec in risus at lacus euismod tristique ac vel mauris. Sed vitae mauris 
-          augue. Donec non auctor eros. Suspendisse potenti. Ut vulputate tortor id tortor iaculis eleifend. Sed lectus 
-          felis, aliquet at hendrerit id, aliquam a metus. Aenean viverra ante magna, vel fringilla nisl luctus eu. Proin sodales 
-          nisl vitae ullamcorper commodo. Nulla eleifend neque vel arcu commodo suscipit. Ut in orci suscipit, vulputate metus eu, 
-          consectetur orci.
-          Donec laoreet metus ac quam dictum, sit amet sollicitudin dolor pretium. Aenean cursus semper purus, a mollis lectus 
-          imperdiet nec. Maecenas ac lacinia ligula, eget fringilla eros. Sed eleifend massa mauris, in aliquet enim malesuada sit 
-          amet. Donec tincidunt ac augue ac congue. Donec rhoncus tincidunt nisl eget pulvinar. Pellentesque at arcu dictum, commodo 
-          ante vitae, scelerisque sapien. In ac sapien efficitur, vestibulum libero in, aliquet nisi. Nulla molestie egestas justo, et 
-          pellentesque nisl ullamcorper id.
+          Full stack developer with a passion for learning. With experience in Ruby on Rails and JavaScript based programming 
+          and a background in Computer Repair and Information Technology, I discovered web development through running my own IT 
+          usiness and building Wordpress sites. I bring strong skills in problem solving and systematic improvement to support 
+          businesses in attaining their short and long term goals.
         </p>
         </div>
     </div>
