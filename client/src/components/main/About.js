5775c2e650deea4a44e47709d371baa80175e392
Mikyle A Crawford <crawfordmikyle@gmail.com>
2018-01-24 13:28:07 -0800

    working on css

--- client/src/components/main/About.js
+++ client/src/components/main/About.js
@@ -1,4 +1,11 @@
 import React from 'react'
+//import skill card logos
+import htmlLogo from '../../media/html.svg'
+import cssLogo from '../../media/css.svg'
+import rubyLogo from '../../media/ruby.svg'
+import javaScriptLogo from '../../media/javascript.svg'
+import railsLogo from '../../media/rails.svg'
+import reactLogo from '../../media/react.svg'
 
 const About = () => {
   return(
@@ -11,11 +18,33 @@ const About = () => {
           usiness and building Wordpress sites. I bring strong skills in problem solving and systematic improvement to support 
           businesses in attaining their short and long term goals.
         </p>
+
         <h5 className='subHeading'>Skills</h5>
-        <div className='skills'>
-          <div className='skillCard'>
+
+        <div className='skills row'>
+          <div className='skillCard four columns' id='html'>
+            <img src={htmlLogo} alt="missing" className='skillLogo'/>
+          </div>
+          <div className='skillCard four columns' id='ruby'>
+            <img src={rubyLogo} alt="missing" className='skillLogo'/>
+          </div>
+          <div className='skillCard four columns' id='JavaScript'>
+            <img src={javaScriptLogo} alt="missing" className='skillLogo'/>
           </div>
         </div>
+
+        <div className='skills row'>
+          <div className='skillCard four columns' id='rails'>
+            <img src={railsLogo} alt="missing" className='skillLogo'/>
+          </div>
+          <div className='skillCard four columns' id='css'>
+            <img src={cssLogo} alt="missing" className='skillLogo'/>
+          </div>
+          <div className='skillCard four columns' id='react'>
+            <img src={reactLogo} alt="missing" className='skillLogo'/>
+          </div>
+        </div>
+
       </div>
     </div>
   )
