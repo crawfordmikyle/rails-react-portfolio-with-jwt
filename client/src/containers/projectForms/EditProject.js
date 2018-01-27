1690f4edf5691816c918df0dd39608b0d25b2df5
Mikyle A Crawford <crawfordmikyle@gmail.com>
2018-01-27 12:58:51 -0800

    working on admin forms

--- client/src/containers/projectForms/EditProject.js
+++ client/src/containers/projectForms/EditProject.js
@@ -20,16 +20,22 @@ class EditProject extends Component {
     })
   };
 
-  handleOnSubmit = (event) => {
+  updateProject = (event) => {
     event.preventDefault();
     this.props.editProjectApi(this.state)
     this.props.history.push('/admin')
   };
-
+  
+  deleteProject = (event) => {
+    event.preventDefault();
+    this.props.deleteProjectApi(this.state)
+    this.props.history.push('/admin')
+  };
+  
   render(){
     return(
       <div className='container'>
-        <form onSubmit={(event)=>this.handleOnSubmit(event)}>
+        <form onSubmit={event => this.updateProject(event)}>
           <label htmlFor="title">Title:</label>
           <input type="text" id="title" value={this.state.title} onChange={(event)=>this.handleOnChange(event)}/>
           <br/>
@@ -40,7 +46,10 @@ class EditProject extends Component {
           <label htmlFor="github_url">GitHub Url:</label>
           <input type="text" id="github_url" value={this.state.github_url} onChange={(event)=>this.handleOnChange(event)}/>
           <br/>
-          <input type="submit"/>
+          <input type="submit" value='Update Project'/>
+        </form>
+        <form onSubmit={event => this.deleteProject(event)}>
+          <input type="submit" value='Delete Project'/>
         </form>
       </div>
     )
