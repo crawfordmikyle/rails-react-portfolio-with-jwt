c5c73f620222807a50bdf4a5df74a3a75af6c6d5
Mikyle A Crawford <crawfordmikyle@gmail.com>
2018-02-08 10:38:35 -0800

    fixed external github links

--- client/src/components/main/ProjectListCard.js
+++ client/src/components/main/ProjectListCard.js
@@ -7,7 +7,7 @@ const BlogPostListCard = ({projectData}) => {
       <img src={projectImg} alt="" className="projectCardImg"/>
       <h5 className="projectTitle">{projectData.title}</h5>
       <p className="projectDescription">{projectData.description}</p>
-      <a className='projectLink' href="">GitHub Link</a>
+      <a href={'http://'+projectData.github_url} target="blank" className='projectLink' >GitHub Link</a>
     </div>
   )
 };
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 user = User.create([{first_name:"Mikyle",last_name:"Crawford",about:"about",github_url:"www.github.com",email:"crawfordmikyle@gmail.com",password:"password"}])
-projects = Project.create([{title: "Test Project One",description:"project description"},{title: "Test Project Two",description:"project description"},{title: "Test Project Three",description:"project description"}])
+
+projects = Project.create([{title: "Test Project One",description:"project description",github_url:"www.github.com"},{title: "Test Project Two",description:"project description",github_url:"www.github.com"},{title: "Test Project Three",description:"project description",github_url:"www.github.com"}])
+
 blog_posts = BlogPost.create([{title: "Test Blog Post One", body:"blog post body"},{title: "Test Blog Post Two", body:"blog post body"},{title: "Test Blog Post Three", body:"blog post body"}])
+
 messages = Message.create([{name: "Test Sender One", email: "test@test.com", phone:"206-222-2222", message_text:"test message content"},{name: "Test Sender One", email: "test@test.com", phone:"206-222-2222", message_text:"test message content"},{name: "Test Sender One", email: "test@test.com", phone:"206-222-2222", message_text:"test message content"}])
