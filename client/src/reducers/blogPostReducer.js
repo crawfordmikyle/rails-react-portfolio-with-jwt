eb93d0b04c3f8e687aa2b7adfde5e58817c99d88
Mikyle A Crawford <crawfordmikyle@gmail.com>
2018-02-19 13:32:40 -0800

    added cases for the reducers

--- client/src/reducers/blogPostReducer.js
+++ client/src/reducers/blogPostReducer.js
@@ -1,9 +1,13 @@
 export default function blogPostReducer(state = [],action){
   switch(action.type){
     case "GET_BLOG_POSTS":
-      return action.blogPosts
+      return action.blogPosts;
     case "ADD_BLOG_POST":
-      return state.concat(action.blogPost)
+      return state.concat(action.blogPost);
+    case "EDIT_BLOG_POST":
+      return state;
+    case "DELETE_BLOG_POST":
+      return state;
     default:
       return state;
   }
--- client/src/reducers/messageReducer.js
+++ client/src/reducers/messageReducer.js
@@ -1,7 +1,11 @@
 export default function messageReducer(state = [],action){
   switch(action.type){
     case 'GET_MESSAGES':
-      return action.messages
+      return action.messages;
+    case 'ADD_MESSAGE':
+      return state;
+    case 'DELETE_MESSAGE':
+      return state;
     default:
       return state;
   }
