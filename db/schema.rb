5364a38ea58e2d88870edd6d4e7c4b13471c1993
Mikyle A Crawford <crawfordmikyle@gmail.com>
2018-03-19 19:00:13 -0700

    working on project categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180210191427) do
+ActiveRecord::Schema.define(version: 20180320015343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20180210191427) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_projects", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "project_id"
   end
 
   create_table "messages", force: :cascade do |t|
